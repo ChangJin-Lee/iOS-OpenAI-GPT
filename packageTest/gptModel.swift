d0adb97cea9803b815ad0b1c9c10c531df72750d
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-25 22:38:10 +0900

    update package

--- packageTest/gptModel.swift
+++ packageTest/gptModel.swift
@@ -6,10 +6,53 @@
 //
 
 import Foundation
+import OpenAI
 
+// MARK: - Model
+struct AIChat: Codable {
+    let datetime: String
+    var issue: String
+    var answer: String?
+    var isResponse: Bool = false
+    var model: String
+    var userAvatarUrl: String
+}
 
-class ChatRoomStore: ObservableObject {
+class ChatGPT: ObservableObject {
 
-    public static let shared = ChatRoomStore()
+    @MainActor
+    func queryGPT(prompts: String, completion: @escaping (String) -> Void) {
+        let configuration = OpenAI.Configuration(token: "", organizationIdentifier: "", timeoutInterval: 60.0)
+        let openAI = OpenAI(configuration: configuration)
+        let customPrompt = prompts
+
+        let query = ChatQuery(model: .gpt3_5Turbo, messages: [.init(role: .user, content: customPrompt)])
+
+        openAI.chatsStream(query: query) { partialResult in
+            print("data:")
+            print(partialResult)
+            switch partialResult {
+            case .success(let result):
+                if let res = result.choices.first?.delta.content{
+                    DispatchQueue.main.async {
+                        completion(res)
+                    }
+                }
+            case .failure(let error):
+                print(error)
+                let errorMessage = error.localizedDescription
+                DispatchQueue.main.async {
+                    completion(errorMessage)
+                }
+            }
+        } completion: { error in
+            print(error ?? "Unknown Error.")
+            if let errorMessage = error?.localizedDescription {
+                DispatchQueue.main.async {
+                    completion(errorMessage)
+                }
+            }
+        }
+    }
 
 }
